9e7a47eb14e23a81c06fc777bae0017cc51705f8
Tony Clonier <clonier.tony@hotmail.fr>
2020-09-26 17:48:59 +0200

    add comments to get_env_or_exception

--- challenge_groover/common/util.py
+++ challenge_groover/common/util.py
@@ -3,6 +3,18 @@ import os
 
 
 def get_env_or_exception(key):
+    """
+    Gets an environnement variable. Raises an exception if it doesn't exist.
+
+    :param key: env variable key
+    :type key: str
+
+    :returns: env variable value
+    :rtype: str
+
+    :raises: django.core.exceptions.ImproperlyConfigured if env variable doesn't exist
+    """
+    
     value = os.getenv(key)
     if value is None:
         raise ImproperlyConfigured(f'{key} env variable is not set')
